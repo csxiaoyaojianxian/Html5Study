dc6f4a82eae58af9ae42bad914617a1613cd8e1d
csxiaoyaojianxian <1724338257@qq.com>
2019-02-05 21:00:28 +0800

    特效

---
+++ 07-常用特效/27-页面切换特效/js/pagetransitions.js
@@ -0,0 +1,384 @@
+var PageTransitions = (function() {
+
+	var $main = $( '#pt-main' ),
+		$pages = $main.children( 'div.pt-page' ), // n个页面元素
+		$iterate = $( '#iterateEffects' ), // 切换按钮
+		animcursor = 1, // 动画类型索引 1～67
+		pagesCount = $pages.length,
+		current = 0, // 当前page索引
+		isAnimating = false, // 开关，控制避免动画重复触发
+		endCurrPage = false, // 开关，上一页动画完成
+		endNextPage = false,
+		animEndEventNames = {
+			'WebkitAnimation' : 'webkitAnimationEnd',
+			'OAnimation' : 'oAnimationEnd',
+			'msAnimation' : 'MSAnimationEnd',
+			'animation' : 'animationend'
+		},
+		// animation end event name
+		animEndEventName = animEndEventNames[ Modernizr.prefixed( 'animation' ) ],
+		// support css animations
+		support = Modernizr.cssanimations;
+	
+	function init() {
+		// 在节点保存原先的class
+		$pages.each( function() {
+			var $page = $( this );
+			$page.data( 'originalClassList', $page.attr( 'class' ) );
+			console.log($page.data( 'originalClassList'))
+		} );
+		// 当前页面
+		$pages.eq( current ).addClass( 'pt-page-current' );
+		// 切换动画按钮(非重点)
+		$( '#dl-menu' ).dlmenu( {
+			animationClasses : { in : 'dl-animate-in-2', out : 'dl-animate-out-2' },
+			onLinkClick : function( el, ev ) {
+				ev.preventDefault();
+				nextPage( el.data( 'animation' ) );
+			}
+		} );
+		// 绑定切换按钮
+		$iterate.on( 'click', function() {
+			if( isAnimating ) {
+				return false;
+			}
+			if( animcursor > 67 ) {
+				animcursor = 1;
+			}
+			nextPage( animcursor );
+			++animcursor;
+		} );
+
+	}
+	// 切换下一页，传入切换动画索引
+	function nextPage( animation ) {
+		// 正在上一个动画中
+		if( isAnimating ) {
+			return false;
+		}
+
+		isAnimating = true;
+		// 旧的当前页
+		var $currPage = $pages.eq( current );
+		// 更新current
+		if( current < pagesCount - 1 ) {
+			++current;
+		} else {
+			current = 0;
+		}
+		// 设置新的当前页
+		var $nextPage = $pages.eq( current ).addClass( 'pt-page-current' ),
+			outClass = '', inClass = '';
+
+		switch( animation ) {
+
+			case 1:
+				outClass = 'pt-page-moveToLeft';
+				inClass = 'pt-page-moveFromRight';
+				break;
+			case 2:
+				outClass = 'pt-page-moveToRight';
+				inClass = 'pt-page-moveFromLeft';
+				break;
+			case 3:
+				outClass = 'pt-page-moveToTop';
+				inClass = 'pt-page-moveFromBottom';
+				break;
+			case 4:
+				outClass = 'pt-page-moveToBottom';
+				inClass = 'pt-page-moveFromTop';
+				break;
+			case 5:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromRight pt-page-ontop';
+				break;
+			case 6:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromLeft pt-page-ontop';
+				break;
+			case 7:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromBottom pt-page-ontop';
+				break;
+			case 8:
+				outClass = 'pt-page-fade';
+				inClass = 'pt-page-moveFromTop pt-page-ontop';
+				break;
+			case 9:
+				outClass = 'pt-page-moveToLeftFade';
+				inClass = 'pt-page-moveFromRightFade';
+				break;
+			case 10:
+				outClass = 'pt-page-moveToRightFade';
+				inClass = 'pt-page-moveFromLeftFade';
+				break;
+			case 11:
+				outClass = 'pt-page-moveToTopFade';
+				inClass = 'pt-page-moveFromBottomFade';
+				break;
+			case 12:
+				outClass = 'pt-page-moveToBottomFade';
+				inClass = 'pt-page-moveFromTopFade';
+				break;
+			case 13:
+				outClass = 'pt-page-moveToLeftEasing pt-page-ontop';
+				inClass = 'pt-page-moveFromRight';
+				break;
+			case 14:
+				outClass = 'pt-page-moveToRightEasing pt-page-ontop';
+				inClass = 'pt-page-moveFromLeft';
+				break;
+			case 15:
+				outClass = 'pt-page-moveToTopEasing pt-page-ontop';
+				inClass = 'pt-page-moveFromBottom';
+				break;
+			case 16:
+				outClass = 'pt-page-moveToBottomEasing pt-page-ontop';
+				inClass = 'pt-page-moveFromTop';
+				break;
+			case 17:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-moveFromRight pt-page-ontop';
+				break;
+			case 18:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-moveFromLeft pt-page-ontop';
+				break;
+			case 19:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-moveFromBottom pt-page-ontop';
+				break;
+			case 20:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-moveFromTop pt-page-ontop';
+				break;
+			case 21:
+				outClass = 'pt-page-scaleDown';
+				inClass = 'pt-page-scaleUpDown pt-page-delay300';
+				break;
+			case 22:
+				outClass = 'pt-page-scaleDownUp';
+				inClass = 'pt-page-scaleUp pt-page-delay300';
+				break;
+			case 23:
+				outClass = 'pt-page-moveToLeft pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 24:
+				outClass = 'pt-page-moveToRight pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 25:
+				outClass = 'pt-page-moveToTop pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 26:
+				outClass = 'pt-page-moveToBottom pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 27:
+				outClass = 'pt-page-scaleDownCenter';
+				inClass = 'pt-page-scaleUpCenter pt-page-delay400';
+				break;
+			case 28:
+				outClass = 'pt-page-rotateRightSideFirst';
+				inClass = 'pt-page-moveFromRight pt-page-delay200 pt-page-ontop';
+				break;
+			case 29:
+				outClass = 'pt-page-rotateLeftSideFirst';
+				inClass = 'pt-page-moveFromLeft pt-page-delay200 pt-page-ontop';
+				break;
+			case 30:
+				outClass = 'pt-page-rotateTopSideFirst';
+				inClass = 'pt-page-moveFromTop pt-page-delay200 pt-page-ontop';
+				break;
+			case 31:
+				outClass = 'pt-page-rotateBottomSideFirst';
+				inClass = 'pt-page-moveFromBottom pt-page-delay200 pt-page-ontop';
+				break;
+			case 32:
+				outClass = 'pt-page-flipOutRight';
+				inClass = 'pt-page-flipInLeft pt-page-delay500';
+				break;
+			case 33:
+				outClass = 'pt-page-flipOutLeft';
+				inClass = 'pt-page-flipInRight pt-page-delay500';
+				break;
+			case 34:
+				outClass = 'pt-page-flipOutTop';
+				inClass = 'pt-page-flipInBottom pt-page-delay500';
+				break;
+			case 35:
+				outClass = 'pt-page-flipOutBottom';
+				inClass = 'pt-page-flipInTop pt-page-delay500';
+				break;
+			case 36:
+				outClass = 'pt-page-rotateFall pt-page-ontop';
+				inClass = 'pt-page-scaleUp';
+				break;
+			case 37:
+				outClass = 'pt-page-rotateOutNewspaper';
+				inClass = 'pt-page-rotateInNewspaper pt-page-delay500';
+				break;
+			case 38:
+				outClass = 'pt-page-rotatePushLeft';
+				inClass = 'pt-page-moveFromRight';
+				break;
+			case 39:
+				outClass = 'pt-page-rotatePushRight';
+				inClass = 'pt-page-moveFromLeft';
+				break;
+			case 40:
+				outClass = 'pt-page-rotatePushTop';
+				inClass = 'pt-page-moveFromBottom';
+				break;
+			case 41:
+				outClass = 'pt-page-rotatePushBottom';
+				inClass = 'pt-page-moveFromTop';
+				break;
+			case 42:
+				outClass = 'pt-page-rotatePushLeft';
+				inClass = 'pt-page-rotatePullRight pt-page-delay180';
+				break;
+			case 43:
+				outClass = 'pt-page-rotatePushRight';
+				inClass = 'pt-page-rotatePullLeft pt-page-delay180';
+				break;
+			case 44:
+				outClass = 'pt-page-rotatePushTop';
+				inClass = 'pt-page-rotatePullBottom pt-page-delay180';
+				break;
+			case 45:
+				outClass = 'pt-page-rotatePushBottom';
+				inClass = 'pt-page-rotatePullTop pt-page-delay180';
+				break;
+			case 46:
+				outClass = 'pt-page-rotateFoldLeft';
+				inClass = 'pt-page-moveFromRightFade';
+				break;
+			case 47:
+				outClass = 'pt-page-rotateFoldRight';
+				inClass = 'pt-page-moveFromLeftFade';
+				break;
+			case 48:
+				outClass = 'pt-page-rotateFoldTop';
+				inClass = 'pt-page-moveFromBottomFade';
+				break;
+			case 49:
+				outClass = 'pt-page-rotateFoldBottom';
+				inClass = 'pt-page-moveFromTopFade';
+				break;
+			case 50:
+				outClass = 'pt-page-moveToRightFade';
+				inClass = 'pt-page-rotateUnfoldLeft';
+				break;
+			case 51:
+				outClass = 'pt-page-moveToLeftFade';
+				inClass = 'pt-page-rotateUnfoldRight';
+				break;
+			case 52:
+				outClass = 'pt-page-moveToBottomFade';
+				inClass = 'pt-page-rotateUnfoldTop';
+				break;
+			case 53:
+				outClass = 'pt-page-moveToTopFade';
+				inClass = 'pt-page-rotateUnfoldBottom';
+				break;
+			case 54:
+				outClass = 'pt-page-rotateRoomLeftOut pt-page-ontop';
+				inClass = 'pt-page-rotateRoomLeftIn';
+				break;
+			case 55:
+				outClass = 'pt-page-rotateRoomRightOut pt-page-ontop';
+				inClass = 'pt-page-rotateRoomRightIn';
+				break;
+			case 56:
+				outClass = 'pt-page-rotateRoomTopOut pt-page-ontop';
+				inClass = 'pt-page-rotateRoomTopIn';
+				break;
+			case 57:
+				outClass = 'pt-page-rotateRoomBottomOut pt-page-ontop';
+				inClass = 'pt-page-rotateRoomBottomIn';
+				break;
+			case 58:
+				outClass = 'pt-page-rotateCubeLeftOut pt-page-ontop';
+				inClass = 'pt-page-rotateCubeLeftIn';
+				break;
+			case 59:
+				outClass = 'pt-page-rotateCubeRightOut pt-page-ontop';
+				inClass = 'pt-page-rotateCubeRightIn';
+				break;
+			case 60:
+				outClass = 'pt-page-rotateCubeTopOut pt-page-ontop';
+				inClass = 'pt-page-rotateCubeTopIn';
+				break;
+			case 61:
+				outClass = 'pt-page-rotateCubeBottomOut pt-page-ontop';
+				inClass = 'pt-page-rotateCubeBottomIn';
+				break;
+			case 62:
+				outClass = 'pt-page-rotateCarouselLeftOut pt-page-ontop';
+				inClass = 'pt-page-rotateCarouselLeftIn';
+				break;
+			case 63:
+				outClass = 'pt-page-rotateCarouselRightOut pt-page-ontop';
+				inClass = 'pt-page-rotateCarouselRightIn';
+				break;
+			case 64:
+				outClass = 'pt-page-rotateCarouselTopOut pt-page-ontop';
+				inClass = 'pt-page-rotateCarouselTopIn';
+				break;
+			case 65:
+				outClass = 'pt-page-rotateCarouselBottomOut pt-page-ontop';
+				inClass = 'pt-page-rotateCarouselBottomIn';
+				break;
+			case 66:
+				outClass = 'pt-page-rotateSidesOut';
+				inClass = 'pt-page-rotateSidesIn pt-page-delay200';
+				break;
+			case 67:
+				outClass = 'pt-page-rotateSlideOut';
+				inClass = 'pt-page-rotateSlideIn';
+				break;
+
+		}
+		// 旧的当前页（上一页）添加退场动画，并绑定动画结束事件
+		$currPage.addClass( outClass ).on( animEndEventName, function() {
+			$currPage.off( animEndEventName ); // 进入后解绑事件
+			endCurrPage = true; // 用于控制两边动画都结束
+			if( endNextPage ) { // 用于控制两边动画都结束，最终执行一出
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+		// 为下一页添加入场动画
+		$nextPage.addClass( inClass ).on( animEndEventName, function() {
+			$nextPage.off( animEndEventName );
+			endNextPage = true; // 用于控制两边动画都结束
+			if( endCurrPage ) { // 用于控制两边动画都结束，最终执行一出
+				onEndAnimation( $currPage, $nextPage );
+			}
+		} );
+
+		if( !support ) {
+			onEndAnimation( $currPage, $nextPage );
+		}
+
+	}
+	// 动画结束，重置参数
+	function onEndAnimation( $outpage, $inpage ) {
+		endCurrPage = false;
+		endNextPage = false;
+		resetPage( $outpage, $inpage );
+		isAnimating = false;
+	}
+	// 动画结束后，更新当前页
+	function resetPage( $outpage, $inpage ) {
+		$outpage.attr( 'class', $outpage.data( 'originalClassList' ) );
+		$inpage.attr( 'class', $inpage.data( 'originalClassList' ) + ' pt-page-current' );
+	}
+
+	init();
+
+	return { init : init };
+
+})();
